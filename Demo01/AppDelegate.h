3afce889089d45c67eb11caf47643f206168de1d
俞家模 <yujiamo@DEO-MacBook-Pro.local>
2015-10-09 12:12:43 +0800

    Initial Commit

---
+++ Demo01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo01
+//
+//  Created by 俞家模 on 15/10/9.
+//  Copyright (c) 2015年 deo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo01
+//
+//  Created by 俞家模 on 15/10/9.
+//  Copyright (c) 2015年 deo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
